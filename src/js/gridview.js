3d666dd6ba4ae06dcd23b6ae8cc986f7e30bdd33
Artem Poletsky <poletsky@immo.ru>
2013-11-19 13:59:47 +0400

    floats

--- src/js/gridview.js
+++ src/js/gridview.js
@@ -77,9 +77,10 @@
             index = (rowsPass + row) * cols;
 
 
-
-
+            console.log(i1, i2, index);
+            
             var children = _.toArray(el.children);
+            
             for (var i = i1; i < i2; i++) {
                 if (index >= collectionLength) {
                     break;
@@ -91,11 +92,11 @@
                 var $view = getView(index, collection, template, $fastJq);
                 $el[method]($view);
 
-
-                $view.css({
-                    "left": viewWidth * col,
-                    "top": (rowsPass + row) * viewHeight
-                });
+                /*
+                 $view.css({
+                 "left": viewWidth * col,
+                 "top": (rowsPass + row) * viewHeight
+                 });*/
 
                 index++;
                 col++;
@@ -108,8 +109,8 @@
         }
 
 
-
-        el.scrollTop = -y;
+        //console.log()
+        el.scrollTop = -rowsPass * viewHeight - y;
         //$el.scrollTop(-y);
 
 
@@ -121,49 +122,56 @@
 
 
     ViewModel.binds.gridView = function($el, value, context, addArgs) {
-        var numItems = 1000;
+        var numItems = 20;
         var width = $el.width();
         var height = $el.height();
 
 
         var collection = [];
-        collection.length = 1000;
-        var viewWidth = 200;
-        var viewHeight = 200;
+        collection.length = numItems;
+        var viewWidth = 2000;
+        var viewHeight = $el.children().height();
         var cols = Math.ceil(width / viewWidth);
         viewWidth = width / cols;
         //console.log(width, viewWidth, cols)
         var rows = Math.ceil(height / viewHeight) + 1;
 
-        var visibleItems = rows * cols;
+        var totalRows = Math.ceil(numItems / cols);
+
+        var maxY = (totalRows) * viewHeight - height;
+        if (maxY < 0)
+            maxY = 0;
+
+        var visibleItems = Math.min(rows * cols, numItems);
+
         var template = $el.html();
 
         $el.empty();
 
-        
-
-        var i = 0;
-        for (var y = 0; y < rows; y++) {
-            for (var x = 0; x < cols; x++) {
 
 
-                var $view = getView(i, null, template, null);
+        var row = 0, col = 0;
+        for (var i = 0; i < visibleItems; i++) {
 
-                $el.append($view);
-                $view.css({
-                    width: viewWidth,
-                    height: viewHeight,
-                    left: x * viewWidth,
-                    top: y * viewHeight
-                });
-                i++;
+            var $view = getView(i, collection, template, null);
+            $el.append($view);
+
+            col++;
+            if (col >= cols) {
+                col = 0;
+                row++;
             }
+
+            $view.css({
+                width: viewWidth
+            });
+
         }
 
         $el.scrollTop(0);
 
 
-        
+
 
 
         var drag = false;
@@ -184,8 +192,9 @@
                     var newX = e.screenX - dx, newY = e.screenY - dy;
 
                     newX = minMax(newX, -Infinity, 0);
-                    newY = minMax(newY, -Infinity, 0);
+                    newY = minMax(newY, -maxY, 0);
 
+                    //console.log(newY, -maxY);
                     setXY($el,
                             newX, newY, x, y,
                             viewWidth, viewHeight,
@@ -197,7 +206,7 @@
             }
         });
 
-        var top = 0, lastTop = 0, step = -150;
+        var top = 0, lastTop = 0, step = -1020;
 
 
         (function draw() {
@@ -210,7 +219,7 @@
                     collection, null);
             lastTop = top;
             window.requestAnimationFrame(draw);
-        }());
+        });
 
     };
 
